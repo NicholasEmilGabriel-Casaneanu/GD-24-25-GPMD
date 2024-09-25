3473563099599e064cd0b0fe50cb0fb0a2be1bb4
Nicholas Casaneanu <nicholascasaneanu508@gmail.com>
2024-09-25 13:41:30 +0100

    Completed practical 3

--- Practical3/2024lab3.cpp
+++ Practical3/2024lab3.cpp
@@ -3,12 +3,12 @@
 int main()
 {
 	
-	char code[1000] = "int main() \n{ \n int var1=10;\n int var2 =10;\n if(var1<10) \n {\n int var3=20;\n} \n}\ n";
-
+	//char code[1000] = "int main() \n{ \n int var1=10;\n int var2 =10;\n if(var1<10) \n {\n int var3=20;\n} \n}\ n";
+	char code[1000] = "int main()\n{ \n int numElephants = 10; \n int var2 = 10; \n	if (var2 < 10)\n{\nint var3 = 20; \n}\n while (var2 < 0)\n { \n if (var2 == 10) \n { \n int var4; \n } \n	int num = 10; \n } \n int var5 = 10; \n }";
 	char variables[1000]="";
 	
-	FindAllVariables(variables, code);
-	FindAllVariablesInScope(variables, code, 12);
-
+	//FindAllVariables(variables, code);
+	FindAllVariablesInScope(variables, code, 17);
+	std::cin.get();
 
 }
--- Practical3/2024lab3.h
+++ Practical3/2024lab3.h
@@ -53,5 +53,100 @@ void FindAllVariables(char variables[1000], char code[1000])
 }
 void FindAllVariablesInScope(char variables[1000], char code[1000], int lineNumber)
 {
+	int codeIndex = 0;
+	int variablesCounter = 0;
+	int currentLine = 1;
 
+	bool done = false;
+	bool reachedScope = false;
+	bool recording = false;
+	int openedBlocks = 0;
+
+	int scope = 0;
+	int destScope = 0;
+	int closeCounter = 0;
+
+	while (!done)
+	{
+		if (!reachedScope)
+		{
+			if (code[codeIndex] == '{')
+			{
+				destScope++;
+			}
+			if (code[codeIndex] == '}')
+			{
+				destScope--;
+				closeCounter++;
+			}
+
+			if (codeIndex > 0 && code[codeIndex] == '\n')
+			{
+				currentLine++;
+			}
+			if (currentLine == lineNumber)
+			{
+				reachedScope = true;
+				codeIndex = 0;
+				currentLine = 1;
+			}
+		}
+		else
+		{
+			if (code[codeIndex] == 123)
+				scope++;
+			if (code[codeIndex] == 125)
+			{
+				if (scope > 0)
+				{
+					scope--;
+					closeCounter--;
+				}
+				else
+				{
+					done = true;
+					break;
+				}
+			}
+			if (recording && 
+				!(
+					(code[codeIndex] < 97 || code[codeIndex] > 90) && (code[codeIndex] < 48 || code[codeIndex] > 57) && (code[codeIndex] < 97 || code[codeIndex] > 122) && (code[codeIndex] < 64)
+				))
+			{
+				variables[variablesCounter] = code[codeIndex];
+				variablesCounter++;
+			}
+			else if (recording &&
+				
+						(
+							(code[codeIndex] < 97 || code[codeIndex] > 90) && (code[codeIndex] < 48 || code[codeIndex] > 57) && (code[codeIndex] < 97 || code[codeIndex] > 122)
+						)
+					)
+			{
+				variables[variablesCounter] = 44;
+				variablesCounter++;
+				recording = false;
+			}
+			if (((code[codeIndex] == 116 && code[codeIndex - 1] == 110 && code[codeIndex - 2] == 105) ||
+				(code[codeIndex] == 114 && code[codeIndex - 1] == 97 && code[codeIndex - 2] == 104 && code[codeIndex - 3] == 99)) &&
+				(scope == 1 || (scope > 1 && closeCounter == 0)))
+			{
+				recording = true;
+				codeIndex++;
+			}
+			if (codeIndex > 0 && code[codeIndex] == '\n')
+			{
+				currentLine++;
+			}
+			if (currentLine > lineNumber)
+			{
+				done = true;
+			}
+
+		}
+		if (codeIndex < 1000)
+			codeIndex++;
+		else
+			done = true;
+	}
 }
