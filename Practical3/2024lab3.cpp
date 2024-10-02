3aca73b4c65cada105fbe00a5b29ad2bc90669ac
Nicholas Casaneanu <nicholascasaneanu508@gmail.com>
2024-10-02 14:49:11 +0100

    Finished Lab3

--- Practical3/2024lab3.cpp
+++ Practical3/2024lab3.cpp
@@ -4,12 +4,69 @@ int main()
 {
 	
 	//char code[1000] = "int main() \n{ \n int var1=10;\n int var2 =10;\n if(var1<10) \n {\n int var3=20;\n} \n}\ n";
-	char code[1000] = "int main()\n{ \n int numElephants = 10; \n int var2 = 10; \n	if (var2 < 10)\n{\nint var3 = 20; \n while(var3 < 21) \n {\n int var6 = 10; \n var3++; \n}\n int var7 = 5; \n int var8 = 16;\n}\n while (var2 < 0)\n { \n if (var2 == 10) \n { \n int var4; \n } \n	int num = 10; \n } \n int var5 = 10; \n }";
-	char variables[1000]="";
+	char code[1000] = "int main()\n{ \n int numElephants = 10; \n int var2 = 10; \nif (var2 < 10)\n	{\n	int var3 = 20; \n	while(var3 < 21) \n		{\n		int var6 = 10; \n		var3++; \n		}\n	int var7 = 5; \n	int var8 = 16;\n	}\n while (var2 < 0)\n	{ \n	if (var2 == 10) \n		{ \n		int var4; \n		} \n	int num = 10; \n	} \n int var5 = 10; \n}";
 	
+	std::cout << code << "\n";
+
 	//FindAllVariables(variables, code);
-	FindAllVariablesInScope(variables, code, 10);
-	std::cout << variables << "\n";
+	for (int i = 0; i < 9; i++)
+	{
+		char variables[1000] = "";
+		if(i==0)
+		{
+			std::cout << "\nline 3:\n";
+			FindAllVariablesInScope(variables, code, 3);
+			std::cout << variables << "\n";
+		}
+		if(i == 1)
+		{
+			std::cout << "\nline 4:\n";
+			FindAllVariablesInScope(variables, code, 4);
+			std::cout << variables << "\n";
+		}
+		if (i == 2)
+		{
+			std::cout << "\nline 7:\n";
+			FindAllVariablesInScope(variables, code, 7);
+			std::cout << variables << "\n";
+		}
+		if (i == 3)
+		{
+			std::cout << "\nline 10:\n";
+			FindAllVariablesInScope(variables, code, 10);
+			std::cout << variables << "\n";
+		}
+		if (i == 4)
+		{
+			std::cout << "\nline 13:\n";
+			FindAllVariablesInScope(variables, code, 13);
+			std::cout << variables << "\n";
+		}
+		if (i == 5)
+		{
+			std::cout << "\nline 14:\n";
+			FindAllVariablesInScope(variables, code, 14);
+			std::cout << variables << "\n";
+		}
+		if (i == 6)
+		{
+			std::cout << "\nline 20:\n";
+			FindAllVariablesInScope(variables, code, 20);
+			std::cout << variables << "\n";
+		}
+		if (i == 7)
+		{
+			std::cout << "\nline 22:\n";
+			FindAllVariablesInScope(variables, code, 22);
+			std::cout << variables << "\n";
+		}
+		if (i == 8)
+		{
+			std::cout << "\nline 24:\n";
+			FindAllVariablesInScope(variables, code, 24);
+			std::cout << variables << "\n";
+		}
+	}
 	std::cin.get();
 
 }
--- Practical3/2024lab3.h
+++ Practical3/2024lab3.h
@@ -24,7 +24,7 @@ void FindAllVariables(char variables[1000], char code[1000])
 			}
 			checkCount++;
 		}
-		if (recording && !(
+		if (check && recording && !(
 			(code[i] < 97 || code[i] > 90) && (code[i] < 48 || code[i] > 57) && (code[i] < 97 || code[i] > 122)
 			))
 		{
@@ -50,6 +50,7 @@ void FindAllVariables(char variables[1000], char code[1000])
 			checkCount = i + 1;
 		}
 	}
+
 }
 void FindAllVariablesInScope(char variables[1000], char code[1000], int lineNumber)
 {
@@ -57,14 +58,22 @@ void FindAllVariablesInScope(char variables[1000], char code[1000], int lineNumb
 	int variablesCounter = 0;
 	int currentLine = 1;
 
+	bool check = false;
+	int checkCount = 0;
+
 	bool done = false;
 	bool reachedScope = false;
 	bool recording = false;
 	int openedBlocks = 0;
 
+	int destScopeIndex = 0;
+	int destScopeIndexTruth = 0;
+
 	int scope = 0;
 	int destScope = 0;
 	int closeCounter = 0;
+
+	bool destScopeReachedInCode = false;
 
 	while (!done)
 	{
@@ -73,6 +82,7 @@ void FindAllVariablesInScope(char variables[1000], char code[1000], int lineNumb
 			if (code[codeIndex] == '{')
 			{
 				destScope++;
+				openedBlocks++;
 			}
 			if (code[codeIndex] == '}')
 			{
@@ -87,14 +97,40 @@ void FindAllVariablesInScope(char variables[1000], char code[1000], int lineNumb
 			if (currentLine == lineNumber)
 			{
 				reachedScope = true;
+				destScopeIndex = codeIndex;
+				while (destScopeIndexTruth != 1)
+				{
+					if (code[destScopeIndex] == '{')
+						destScopeIndexTruth++;
+					else if (code[destScopeIndex] == '}')
+						destScopeIndexTruth--;
+					destScopeIndex--;
+				}
 				codeIndex = 0;
 				currentLine = 1;
 			}
 		}
 		else
 		{
+			while (!check && recording)
+			{
+				if (code[checkCount] == 32)
+				{
+					check = true;
+				}
+				else if (code[checkCount] == 40)
+				{
+					check = true;
+					recording = false;
+					codeIndex = checkCount;
+				}
+				checkCount++;
+			}
 			if (code[codeIndex] == '{')
+			{
 				scope++;
+				openedBlocks--;
+			}
 			if (code[codeIndex] == '}')
 			{
 				if (scope > 0)
@@ -110,26 +146,28 @@ void FindAllVariablesInScope(char variables[1000], char code[1000], int lineNumb
 			}
 			if (recording && 
 				!(
-					(code[codeIndex] < 'a' || code[codeIndex] > 'Z') && (code[codeIndex] < '0' || code[codeIndex] > '9') && (code[codeIndex] < 'a' || code[codeIndex] > 'z') && (code[codeIndex] < '@')
+					(code[codeIndex] < 'A' || code[codeIndex] > 'Z') && (code[codeIndex] < '0' || code[codeIndex] > '9') && (code[codeIndex] < 'a' || code[codeIndex] > 'z') && (code[codeIndex] < '@')
 				))
 			{
 				variables[variablesCounter] = code[codeIndex];
 				variablesCounter++;
+				check = false;
 			}
 			else if (recording &&
 				
 						(
-							(code[codeIndex] < 97 || code[codeIndex] > 90) && (code[codeIndex] < 48 || code[codeIndex] > 57) && (code[codeIndex] < 97 || code[codeIndex] > 122)
+							(code[codeIndex] < 65 || code[codeIndex] > 90) && (code[codeIndex] < 48 || code[codeIndex] > 57) && (code[codeIndex] < 97 || code[codeIndex] > 122)
 						)
 					)
 			{
 				variables[variablesCounter] = 44;
 				variablesCounter++;
 				recording = false;
+				checkCount = codeIndex;
 			}
 			if (((code[codeIndex] == 116 && code[codeIndex - 1] == 110 && code[codeIndex - 2] == 105) ||
 				(code[codeIndex] == 114 && code[codeIndex - 1] == 97 && code[codeIndex - 2] == 104 && code[codeIndex - 3] == 99)) &&
-				(scope == 1 || (scope > 1 && closeCounter == 0) || scope == 0))
+				(scope == 1 || scope == 0 || (scope == destScope && (closeCounter < destScope && closeCounter>=0 && openedBlocks == closeCounter && codeIndex > destScopeIndex)) || closeCounter == 0))
 			{
 				recording = true;
 				codeIndex++;
@@ -138,10 +176,11 @@ void FindAllVariablesInScope(char variables[1000], char code[1000], int lineNumb
 			{
 				currentLine++;
 			}
-			/*if (currentLine > lineNumber)
+			if (currentLine > lineNumber)
 			{
 				done = true;
-			}*/
+			}
+			//std::cout << variables << "\n";
 
 		}
 		if (codeIndex < 1000)
